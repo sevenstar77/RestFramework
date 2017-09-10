04a9554bfac4ce954fe0afd04e70f1b429ade976
sevenstar77 <amazonprimeday777@gmail.com>
2017-09-10 22:08:22 +0900

    model & serializers

--- music/models.py
+++ music/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Music(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200, blank=True, default='')
-    reloease_date = models.DateTimeField()
-    game_category = models.CharField(max_length=200, blank=True, default='')
+    release_date = models.DateTimeField()
+    music_category = models.CharField(max_length=200, blank=True, default='')
     played = models.BooleanField(default=False)
 
     class Meta:
